01440bedb09208251e0fac3e802048c86d48dd42
Harsh Bhikadiya <harsh.bhikadiya55@gmail.com>
2023-12-31 23:15:30 +0530

    Sync 2

--- app/src/main/java/com/harsh/discordclone/presentation/mainscreen/MainActivity.kt
+++ app/src/main/java/com/harsh/discordclone/presentation/mainscreen/MainActivity.kt
@@ -1,19 +1,32 @@
 package com.harsh.discordclone.presentation.mainscreen
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import com.harsh.discordclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.root.setOnClickListener {
-            Firebase.auth.signOut()
+//        binding.root.setOnClickListener {
+//            Firebase.auth.signOut()
+//        }
+        binding.mainContainer.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN->{
+                    println("child : action_down")
+                }
+                MotionEvent.ACTION_MOVE->{
+                    println("child : action_move")
+                }
+            }
+            true
         }
     }
 }
